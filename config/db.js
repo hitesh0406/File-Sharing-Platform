346038a306f640b11b464f35868fce2f3c6be6c2
hitesh0406 <hiteshbopche932@gmail.com>
2025-03-05 22:08:35 +0530

    Project completed

--- config/db.js
+++ config/db.js
@@ -5,6 +5,7 @@ const connectDB = async() =>{
     try{
         await mongoose.connect(process.env.MONGO_URI)
         console.log("Connected to MongoDB")
+        console.log("Connection Successful")
     }catch(err){
         console.error("MongoDB connection error:",err);
         process.exit(1);
--- utils/constants.js
+++ utils/constants.js
@@ -1,1 +1,3 @@
 //Stores reusable constants like allowed file types, size limits, error messages, etc
+
+console.log("Nothing here");
